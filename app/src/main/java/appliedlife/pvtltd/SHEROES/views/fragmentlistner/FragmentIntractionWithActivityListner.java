a3e16d2bcc314dd01151fc86bffad81d8f6cd931
Ravi <ravi@sheroes.in>
2017-12-22 16:25:00 +0530

    test

--- app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragmentlistner/FragmentIntractionWithActivityListner.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragmentlistner/FragmentIntractionWithActivityListner.java
@@ -9,5 +9,6 @@ import appliedlife.pvtltd.SHEROES.models.entities.feed.FeedDetail;
 
 public interface FragmentIntractionWithActivityListner {
     void onShowErrorDialog(String errorReason,FeedParticipationEnum feedParticipationEnum);
+    
      void onSuccessResult(String result, FeedDetail feedDetail) ;
 }
