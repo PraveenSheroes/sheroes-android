33ee24786f2ae8a3fde75211f9f66d753805fc1f
ujjwalnitw <ujjwal@sheroes.in>
2018-03-21 17:11:17 +0530

    refactoring

--- app/src/main/java/appliedlife/pvtltd/SHEROES/models/AppInstallation.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/models/AppInstallation.java
@@ -1,48 +1,6 @@
 package appliedlife.pvtltd.SHEROES.models;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.provider.Settings;
-
-import com.crashlytics.android.Crashlytics;
-import com.f2prateek.rx.preferences2.Preference;
-import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TimeZone;
-import java.util.UUID;
-
-import javax.inject.Inject;
-
-import appliedlife.pvtltd.SHEROES.R;
-import appliedlife.pvtltd.SHEROES.basecomponents.SheroesAppModule;
-import appliedlife.pvtltd.SHEROES.basecomponents.SheroesAppServiceApi;
-import appliedlife.pvtltd.SHEROES.basecomponents.SheroesApplication;
-import appliedlife.pvtltd.SHEROES.models.entities.feed.ChallengeSolrObj;
-import appliedlife.pvtltd.SHEROES.models.entities.feed.FeedDetail;
-import appliedlife.pvtltd.SHEROES.models.entities.feed.FeedRequestPojo;
-import appliedlife.pvtltd.SHEROES.models.entities.feed.FeedResponsePojo;
-import appliedlife.pvtltd.SHEROES.models.entities.login.LoginResponse;
-import appliedlife.pvtltd.SHEROES.models.entities.post.Contest;
-import appliedlife.pvtltd.SHEROES.utils.AppConstants;
-import appliedlife.pvtltd.SHEROES.utils.AppUtils;
-import appliedlife.pvtltd.SHEROES.utils.CommonUtil;
-import appliedlife.pvtltd.SHEROES.utils.LogUtils;
-import appliedlife.pvtltd.SHEROES.utils.networkutills.NetworkUtil;
-import appliedlife.pvtltd.SHEROES.utils.stringutils.StringUtil;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by ujjwal on 11/03/18.
@@ -58,10 +16,8 @@ public class AppInstallation {
     @SerializedName("advertising_id")
     public String advertisingId; //done
 
-
     @SerializedName("os_version")
     public String androidVersion; //done
-
 
     @SerializedName("app_version")
     public String appVersion;
