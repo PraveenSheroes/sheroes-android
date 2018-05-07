fc3ae812e1b084566fd12dda2f185625a64f56f8
praveen <praveen@sheroes.in>
2018-05-07 13:44:57 +0530

    index fix
    Change-Id: Icb5db2a41b930c5c05cdf7f3d56ea22580bb45de

--- app/src/main/java/appliedlife/pvtltd/SHEROES/views/viewholders/FeedCommunityPostHolder.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/views/viewholders/FeedCommunityPostHolder.java
@@ -1540,11 +1540,11 @@ public class FeedCommunityPostHolder extends BaseViewHolder<FeedDetail> {
     }
 
     private void showUserMentionName(String description, List<MentionSpan> mentionSpanList, boolean isComment) {
-        StringBuilder strWithAddExtra = new StringBuilder(description + " ");
+        StringBuilder strWithAddExtra = new StringBuilder(description);
         for (int i = 0; i < mentionSpanList.size(); i++) {
             final MentionSpan mentionSpan = mentionSpanList.get(i);
             if (null != mentionSpan && null != mentionSpan.getMention()) {
-                if (mentionSpan.getMention().getStartIndex() >= 0 && mentionSpan.getMention().getStartIndex() + i <= strWithAddExtra.length() - 1) {
+                if (mentionSpan.getMention().getStartIndex() >= 0 && mentionSpan.getMention().getStartIndex() + i < strWithAddExtra.length()) {
                     strWithAddExtra.insert(mentionSpan.getMention().getStartIndex() + i, '@');
                 }
             }
@@ -1577,7 +1577,7 @@ public class FeedCommunityPostHolder extends BaseViewHolder<FeedDetail> {
                     }
                 };
 
-                if (mentionSpan.getMention().getStartIndex() >= 0 && mentionSpan.getMention().getEndIndex() > 0 && mentionSpan.getMention().getEndIndex() + i + 1 <= spannableString.length() && mentionSpan.getMention().getStartIndex() + i <= spannableString.length()) {
+                if (mentionSpan.getMention().getStartIndex() >= 0 && mentionSpan.getMention().getEndIndex() > 0 && mentionSpan.getMention().getEndIndex() + i + 1 < spannableString.length() && mentionSpan.getMention().getStartIndex() + i <= spannableString.length()) {
                     int start = mentionSpan.getMention().getStartIndex() + i;
                     int end = mentionSpan.getMention().getEndIndex() + i;
                     spannableString.setSpan(postedInClick, start, end + 1, 0);
--- app/src/main/java/appliedlife/pvtltd/SHEROES/views/viewholders/UserPostHolder.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/views/viewholders/UserPostHolder.java
@@ -967,11 +967,11 @@ public class UserPostHolder extends BaseViewHolder<FeedDetail> {
     }
 
     private void showUserMentionName(String description, List<MentionSpan> mentionSpanList) {
-        StringBuilder strWithAddExtra = new StringBuilder(description + " ");
+        StringBuilder strWithAddExtra = new StringBuilder(description);
         for (int i = 0; i < mentionSpanList.size(); i++) {
             final MentionSpan mentionSpan = mentionSpanList.get(i);
             if (null != mentionSpan && null != mentionSpan.getMention()) {
-                if (mentionSpan.getMention().getStartIndex()>=0&&mentionSpan.getMention().getStartIndex() + i <= strWithAddExtra.length() - 1) {
+                if (mentionSpan.getMention().getStartIndex()>=0&&mentionSpan.getMention().getStartIndex() + i < strWithAddExtra.length()) {
                     strWithAddExtra.insert(mentionSpan.getMention().getStartIndex() + i, '@');
                 }
             }
@@ -999,7 +999,7 @@ public class UserPostHolder extends BaseViewHolder<FeedDetail> {
                         textPaint.setUnderlineText(false);
                     }
                 };
-                if (mentionSpan.getMention().getStartIndex() >=0&&mentionSpan.getMention().getEndIndex()>0&&mentionSpan.getMention().getEndIndex() + i + 1 <= spannableString.length() && mentionSpan.getMention().getStartIndex() + i <= spannableString.length()) {
+                if (mentionSpan.getMention().getStartIndex() >=0&&mentionSpan.getMention().getEndIndex()>0&&mentionSpan.getMention().getEndIndex() + i + 1 < spannableString.length() && mentionSpan.getMention().getStartIndex() + i <= spannableString.length()) {
                     int start = mentionSpan.getMention().getStartIndex() + i;
                     int end = mentionSpan.getMention().getEndIndex() + i;
                     spannableString.setSpan(postedInClick, start, end + 1, 0);
