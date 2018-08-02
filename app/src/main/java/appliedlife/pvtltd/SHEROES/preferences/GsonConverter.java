5bb936417f7fca4ebd245ddc2310c8640549c6ce
praveen <praveen@sheroes.in>
2018-08-02 12:19:03 +0530

    Outofmemoryerror fix for GSON converter serialize method
    Change-Id: I0e1ea127bc90bf7c408c1c181c7c5a2e93158625

--- app/src/main/java/appliedlife/pvtltd/SHEROES/preferences/GsonConverter.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/preferences/GsonConverter.java
@@ -1,10 +1,13 @@
 
 package appliedlife.pvtltd.SHEROES.preferences;
 
+import android.os.Environment;
 import android.support.annotation.NonNull;
 
 import com.f2prateek.rx.preferences2.Preference;
 import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class GsonConverter<T> implements Preference.Converter<T> {
     final Gson gson;
@@ -14,14 +17,32 @@ public class GsonConverter<T> implements Preference.Converter<T> {
         this.gson = gson;
         this.clazz = clazz;
     }
-    @NonNull @Override public T deserialize(@NonNull String serialized) {
+    @NonNull
+    @Override
+    public T deserialize(@NonNull String serialized) {
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
         return gson.fromJson(serialized, clazz);
     }
 
     @NonNull
     @Override
     public String serialize(@NonNull T t) {
+        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
         json = gson.toJson(t);
         return json;
+    }
+
+    private class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread thread, Throwable ex) {
+            if (ex.getClass().equals(OutOfMemoryError.class)) {
+                try {
+                    android.os.Debug.dumpHprofData(Environment.getExternalStorageState()+"/dump.hprof");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            ex.printStackTrace();
+        }
     }
 }
