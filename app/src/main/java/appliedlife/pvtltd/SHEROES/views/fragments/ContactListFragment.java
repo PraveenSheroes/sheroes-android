7c1c91d2ebe60e910c8fb0bdce6a14845ad15355
Ravi <ravi@sheroes.in>
2018-08-27 16:56:10 +0530

    Fix for NumberFormatException in ContactListFragment
    java.lang.NumberFormatException while converting special char contained phonenumber to long
    
    Replace special char from Phone number

--- app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragments/ContactListFragment.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragments/ContactListFragment.java
@@ -311,7 +311,8 @@ public class ContactListFragment extends BaseFragment implements ContactDetailCa
                     if (!isWhatsAppNumber.contains("91")) {
                         isWhatsAppNumber = "91" + isWhatsAppNumber;
                     }
-                    isWhatsAppNumber = isWhatsAppNumber.replace("+", "").replace(" ", "");
+
+                    isWhatsAppNumber = isWhatsAppNumber.replaceAll("[\\D]", "");
                     boolean isWhatsappInstalled = whatsAppInstalledOrNot(AppConstants.WHATS_APP);
                     if (isWhatsappInstalled) {
                         Intent sendIntent = new Intent("android.intent.action.MAIN");
