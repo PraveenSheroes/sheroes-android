ba4d6e0f9f8d295bb1bd2c26798597b9a71c2672
praveen <praveen@sheroes.in>
2018-10-09 18:27:32 +0530

    textview scroll file refector
    Change-Id: I35bbd4a50282fab1c4a5e957c07ee26956c3523d

--- app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/NoScrollTextView.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/NoScrollTextView.java
@@ -1,21 +1,13 @@
 package appliedlife.pvtltd.SHEROES.vernacular;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Typeface;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
-
-import appliedlife.pvtltd.SHEROES.R;
-import appliedlife.pvtltd.SHEROES.vernacular.LanguageType;
-import appliedlife.pvtltd.SHEROES.vernacular.LocaleManager;
 
 /**
  * Created by Praveen on 07/11/17.
  */
 
-public class NoScrollTextView extends AppCompatTextView {
+public class NoScrollTextView extends CustomTextViewFont {
 
     public NoScrollTextView(Context context) {
         super(context);
@@ -23,35 +15,10 @@ public class NoScrollTextView extends AppCompatTextView {
 
     public NoScrollTextView(Context context, AttributeSet set) {
         super(context, set);
-        setLanguageFont(context, set);
     }
 
     public NoScrollTextView(Context context, AttributeSet set, int defaultStyle) {
         super(context, set, defaultStyle);
-        setLanguageFont(context, set);
-    }
-
-    private void setLanguageFont(Context context, AttributeSet attrs) {
-        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.NoScrollTextViewFont);
-        CharSequence fontFamily = styledAttributes.getString(R.styleable.NoScrollTextViewFont_noScrollTextFontFamily);
-        if (fontFamily != null) {
-            if (!LocaleManager.getLanguage(context).equalsIgnoreCase(LanguageType.ENGLISH.toString())) {
-                Typeface typeface;
-                if (fontFamily.toString().equalsIgnoreCase("regular")) {
-                    typeface = ResourcesCompat.getFont(context, R.font.noto_sans_regular);
-                } else if (fontFamily.toString().equalsIgnoreCase("light")) {
-                    typeface = ResourcesCompat.getFont(context, R.font.noto_sans_regular);
-                } else if (fontFamily.toString().equalsIgnoreCase("medium")) {
-                    typeface = ResourcesCompat.getFont(context, R.font.noto_sans_bold);
-                } else if (fontFamily.toString().equalsIgnoreCase("bold")) {
-                    typeface = ResourcesCompat.getFont(context, R.font.noto_sans_bold);
-                } else {
-                    typeface = ResourcesCompat.getFont(context, R.font.noto_sans_regular);
-                }
-                setTypeface(typeface);
-            }
-        }
-        styledAttributes.recycle();
     }
 
     @Override
