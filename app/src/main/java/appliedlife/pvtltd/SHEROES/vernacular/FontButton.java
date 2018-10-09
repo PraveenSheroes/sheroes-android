dd3d5809f657df6862a8628a4d85f713e31807ed
praveen <praveen@sheroes.in>
2018-10-09 17:57:41 +0530

    Button rename
    Change-Id: I4e0bde5ee8bd4005cb60e5c7876a6ad82bdf9ab1

--- app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/FontButton.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/FontButton.java
@@ -7,29 +7,26 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
-import java.util.Locale;
-
 import appliedlife.pvtltd.SHEROES.R;
-import appliedlife.pvtltd.SHEROES.utils.AppConstants;
 
-public class CustomButtonFont extends AppCompatButton {
-    public CustomButtonFont(Context context) {
+public class FontButton extends AppCompatButton {
+    public FontButton(Context context) {
         super(context);
     }
 
-    public CustomButtonFont(Context context, AttributeSet set) {
+    public FontButton(Context context, AttributeSet set) {
         super(context, set);
         setLanguageFont(context, set);
     }
 
-    public CustomButtonFont(Context context, AttributeSet set, int defaultStyle) {
+    public FontButton(Context context, AttributeSet set, int defaultStyle) {
         super(context, set, defaultStyle);
         setLanguageFont(context, set);
     }
 
     private void setLanguageFont(Context context, AttributeSet attrs) {
-        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.CustomButtonFont);
-        CharSequence fontFamily = styledAttributes.getString(R.styleable.CustomButtonFont_customButtonFontFamily);
+        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.FontButton);
+        CharSequence fontFamily = styledAttributes.getString(R.styleable.FontButton_customButtonFontFamily);
         if (fontFamily != null) {
             if (!LocaleManager.getLanguage(context).equalsIgnoreCase(LanguageType.ENGLISH.toString())) {
                 Typeface typeface;
