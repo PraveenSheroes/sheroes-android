15a312d9b6d08a9bc34794816bcc7845b442eead
praveen <praveen@sheroes.in>
2018-10-09 17:59:51 +0530

    Edit text rename
    Change-Id: I702fa04ab8ea5f8feb85be5c2b360c35c1b06928

--- app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/FontEditText.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/vernacular/FontEditText.java
@@ -7,29 +7,26 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
 
-import java.util.Locale;
-
 import appliedlife.pvtltd.SHEROES.R;
-import appliedlife.pvtltd.SHEROES.utils.AppConstants;
 
-public class CustomEditTextFont extends AppCompatEditText {
-    public CustomEditTextFont(Context context) {
+public class FontEditText extends AppCompatEditText {
+    public FontEditText(Context context) {
         super(context);
     }
 
-    public CustomEditTextFont(Context context, AttributeSet set) {
+    public FontEditText(Context context, AttributeSet set) {
         super(context, set);
         setLanguageFont(context, set);
     }
 
-    public CustomEditTextFont(Context context, AttributeSet set, int defaultStyle) {
+    public FontEditText(Context context, AttributeSet set, int defaultStyle) {
         super(context, set, defaultStyle);
         setLanguageFont(context, set);
     }
 
     private void setLanguageFont(Context context, AttributeSet attrs) {
-        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.CustomEditTextFont);
-        CharSequence fontFamily = styledAttributes.getString(R.styleable.CustomEditTextFont_customEditTextFontFamily);
+        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.FontEditText);
+        CharSequence fontFamily = styledAttributes.getString(R.styleable.FontEditText_customEditTextFontFamily);
         if (fontFamily != null) {
             if (!LocaleManager.getLanguage(context).equalsIgnoreCase(LanguageType.ENGLISH.toString())) {
                 Typeface typeface;
