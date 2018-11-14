fd05182ea9f1eeedeee72fa2d9c767c35032cb3e
praveen <praveen@sheroes.in>
2018-11-14 12:17:12 +0530

    vernacular hindi typing fix
    Change-Id: Ib6ed426194cb49276f94e45ffe7d74c0bb8c4d62

--- app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragments/HelplineFragment.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/views/fragments/HelplineFragment.java
@@ -42,6 +42,7 @@ import appliedlife.pvtltd.SHEROES.presenters.HelplinePresenter;
 import appliedlife.pvtltd.SHEROES.social.GoogleAnalyticsEventActions;
 import appliedlife.pvtltd.SHEROES.utils.AppConstants;
 import appliedlife.pvtltd.SHEROES.utils.AppUtils;
+import appliedlife.pvtltd.SHEROES.utils.CommonUtil;
 import appliedlife.pvtltd.SHEROES.utils.LogUtils;
 import appliedlife.pvtltd.SHEROES.utils.stringutils.StringUtil;
 import appliedlife.pvtltd.SHEROES.vernacular.LocaleManager;
@@ -55,6 +56,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
+import static appliedlife.pvtltd.SHEROES.utils.AppConstants.LANGUAGE_KEY;
 import static appliedlife.pvtltd.SHEROES.utils.AppUtils.helplineGetChatThreadRequestBuilder;
 
 /**
@@ -167,7 +169,7 @@ public class HelplineFragment extends BaseFragment {
         try {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, CommonUtil.getPrefStringValue(LANGUAGE_KEY));
             intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.ID_SPEECH_PROMPT));
             startActivityForResult(intent, AppConstants.REQ_CODE_SPEECH_INPUT);
         } catch(ActivityNotFoundException e) {
