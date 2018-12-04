b9c653e821028a1ae5f141bec34e1d360aab190b
praveen <praveen@sheroes.in>
2018-12-04 16:32:29 +0530

    code review comments: unused code removed
    Change-Id: I215a5abe7731725a8722a89555eb3351c5775e4b

--- app/src/main/java/appliedlife/pvtltd/SHEROES/analytics/Impression/ImpressionHelper.java
+++ app/src/main/java/appliedlife/pvtltd/SHEROES/analytics/Impression/ImpressionHelper.java
@@ -261,11 +261,6 @@ public class ImpressionHelper implements ImpressionTimer.ITimerCallback {
             //Ignore header
             if (feedDetail.getSubType().equalsIgnoreCase(AppConstants.HOME_FEED_HEADER))
                 return null;
-
-            /*if (feedDetail.getSubType().equalsIgnoreCase(AppConstants.CAROUSEL_SUB_TYPE)) {
-                //Add position in list
-            }*/
-
             if (prefs != null && prefs.contains(AppConstants.FEED_CONFIG_VERSION)) {
                 String configVersion = prefs.getString(AppConstants.FEED_CONFIG_VERSION, "0");
                 configVersion = StringUtil.isNotNullOrEmptyString(configVersion) ? configVersion : "0";
@@ -409,8 +404,7 @@ public class ImpressionHelper implements ImpressionTimer.ITimerCallback {
 
     @Override
     public void sendImpressions() {
-        mImpressionPresenter.sendImpressions(mImpressionBatchSize,null, true);
+        mImpressionPresenter.sendImpressions(mImpressionBatchSize, null, true);
     }
-
     //endregion
 }
