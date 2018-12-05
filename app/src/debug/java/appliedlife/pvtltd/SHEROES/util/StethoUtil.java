e583cedc8d0b46c63f20a16efc543faeeed3c0ca
dharmendraSheroes <dharmendra@sheroes.in>
2018-12-05 17:06:56 +0530

    review_changes

--- app/src/debug/java/appliedlife/pvtltd/SHEROES/util/StethoUtil.java
+++ app/src/debug/java/appliedlife/pvtltd/SHEROES/util/StethoUtil.java
@@ -1,9 +1,7 @@
 package appliedlife.pvtltd.SHEROES.util;
 
 import android.app.Application;
-
 import com.facebook.stetho.Stetho;
-
 
 public class StethoUtil {
     public static void initStetho(Application application) {
